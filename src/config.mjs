ae2afb7fcc4c2d52dc8df3d991aab8f82283a5f8
Nikolai Mørck <32653973+nikolaimorck@users.noreply.github.com>
2022-11-11 22:06:33 +0100

    Update config.mjs

--- src/config.mjs
+++ src/config.mjs
@@ -1,12 +1,12 @@
 export const SITE = {
-	name: 'AstroWind',
+	name: 'nxko',
 
-	origin: 'https://astrowind.vercel.app',
+	origin: 'https://nxko.com',
 	basePathname: '/',
 	trailingSlash: false,
 
-	title: 'AstroWind — Your website with Astro + Tailwind CSS',
-	description: '🚀 AstroWind is a free and ready to start template to make your website using Astro and Tailwind CSS.',
+	title: 'nxko - street photographs',
+	description: 'Street photographs ny nxko',
 
 	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
 	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
